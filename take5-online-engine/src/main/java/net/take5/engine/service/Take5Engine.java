fc46697dff164b458abb6ba72980bcadca6dd9bb
KenTiN <quentin.lebourgeois@gmail.com>
2014-11-11 18:16:20 +0100

    Initialisation des premières actions : - Connexion via pseudo - Liste des utilisateurs connectés - Liste des lobbies - Création d'un lobby

--- take5-online-engine/src/main/java/net/take5/engine/service/Take5Engine.java
+++ take5-online-engine/src/main/java/net/take5/engine/service/Take5Engine.java
@@ -1,11 +1,24 @@
 package net.take5.engine.service;
 
+import net.take5.commons.pojo.output.Lobby;
+import net.take5.commons.pojo.output.User;
+
+import org.springframework.stereotype.Service;
+
 /**
  * Interface du moteur de jeu
  * 
  * @author Quentin
  */
+@Service
 public interface Take5Engine
 {
-
+    /**
+     * Permet de créer un lobby
+     * 
+     * @param creator
+     *            créateur du lobby
+     * @return un lobby
+     */
+    Lobby createLobby(User creator);
 }
