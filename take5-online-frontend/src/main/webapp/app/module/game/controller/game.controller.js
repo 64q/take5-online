85841f8bc2364037c6d33b6bc66a2ac41dda25cb
Lulububu <erwan.matrat@cegetel.net>
2014-11-16 21:43:46 +0100

    Affichage basique des cartes sur le plateau de jeu
    Ajout des cartes de différentes couleurs. Récupération des cartes depuis
    le serveur.

--- take5-online-frontend/src/main/webapp/app/module/game/controller/game.controller.js
+++ take5-online-frontend/src/main/webapp/app/module/game/controller/game.controller.js
@@ -4,8 +4,9 @@
 
 var controllers = angular.module('take5Controllers.game', []);
 
-controllers.controller('GameCtrl', [ '$scope', 'WebSocketManagerService', 'ACTION', '$state', 'STATUT',
-		function($scope, WebSocketManagerService, ACTION, $state, STATUT) {
+controllers.controller('GameCtrl', [ '$scope', 'WebSocketManagerService', 'ACTION', '$state', 'STATUT', '$rootScope',
+		function($scope, WebSocketManagerService, ACTION, $state, STATUT, $rootScope) {
 
-
+//	$rootScope.gameBoard = {board : [[{value: 15, oxHeads: 1}, {value: 1, oxHeads: 5}], [{value: 15, oxHeads: 1}, {value: 1, oxHeads: 5}]]};
+//	$rootScope.hand = {cards : [{value: 15, oxHeads: 1}, {value: 1, oxHeads: 5}, {value: 15, oxHeads: 7}, {value: 15, oxHeads: 3}, {value: 15, oxHeads: 1}, {value: 1, oxHeads: 2}, {value: 15, oxHeads: 7}, {value: 15, oxHeads: 3}, {value: 15, oxHeads: 1}, {value: 1, oxHeads: 5}]};
 } ]);
